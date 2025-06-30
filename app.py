fa40767323a7bdc63aeac1fb96119f0a655aa399
Edward Alfonso <edwardalfonsog@gmail.com>
2025-06-30 10:44:21 -0600

    Fix: Aumentar longitud de password_hash a 256

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ class Orden(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='operario') # Roles: operario, admin
 
     def set_password(self, password):
